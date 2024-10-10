0a578ace5a4a4b20db7f84a7c4b6e84fa20e02c8
mhartwig1337 <hartwig@pega-sus.de>
2024-10-10 13:34:10 +0200

    [task] added if(Symbol)

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const typelist = document.querySelectorAll(".link-button-container__type")
+const typelist = document.querySelectorAll(".link-button-container__type");
 const itemlist = document.querySelectorAll(".link-button-container__link");
+const symbollist = document.querySelectorAll(".link-button-container__symbol");
 let n = 0;
 
 const extension = ext => {
     let item = itemlist[n];
+    let symbol = symbollist[n];
     let link = item.getAttribute("href");
-    let end = link.split(".").pop().toUpperCase();
+    let end = link.split(".").pop().toLowerCase();
     typelist[n].innerHTML = "[" + end +"]";
+    if (symbol) {
+        console.log(symbol);
+        console.log(symbollist[n]);
+        /* let link = item.getAttribute("href"); */
+       /*  let end = link.split(".").pop().toLowerCase(); */
+        if (end == "jpg" || end == "png" || end == "pdf" || end == "zip" ) {
+            symbol.src = "image/symbol-" + end + ".png";
+            console.log(symbol.src)
+        }
+        else {
+            symbol.src = "image/symbol-unknown.jpg";
+        }
+        /* typelist[n].innerHTML = "[" + end +"]"; */
+    }
+    
     n++;
+
 }
 
 itemlist.forEach(extension);
